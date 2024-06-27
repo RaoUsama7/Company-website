ddaa58950b77530151361d14e52deb5bae4d6c61
Usama <raorvp20@gmail.com>
2024-06-28 04:27:13 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('video1');
+
+    // Example: Play the video when the page loads
+    video.play();
+});
